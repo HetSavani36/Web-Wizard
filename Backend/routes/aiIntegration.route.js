78c4ec8a63323ca82f7ab2563c5f84c1bdc2d719
HetSavani36 <hetsavani36@gmail.com>
2025-09-30 14:38:25 +0530

    remove extra functions from ai-controller

--- Backend/routes/aiIntegration.route.js
+++ Backend/routes/aiIntegration.route.js
@@ -1,17 +1,13 @@
 import { Router } from "express"
 import {
-  summarizePost,
   moderateComment,
-  getRecommendations,
   translatePost,
 } from "../controllers/aiIntegration.controller.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router()
 
-router.get("/summarize/:postId", summarizePost)
 router.post("/moderate-comment", verifyJWT, moderateComment)
-router.get("/recommendations", verifyJWT, getRecommendations)
 router.get("/translate/:postId", translatePost)
 
 export default router
